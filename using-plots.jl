5cd431aaa00f1c83ede3955bbc9175d5d390fa32
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2021-06-19 21:44:13 -0300

    Create using-plots.jl

---
+++ using-plots.jl
@@ -0,0 +1,8 @@
+using Plots
+x = 1:10
+f(x) = x^2 - 7x
+y = f.(x)
+
+gr()                                # use the GR bacckend
+plot(x, y, label="line")            # plot a line
+scatter!(x, y, label="points")      # mutate previous plot (!) and add points at same chart
