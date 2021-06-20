21dc3d7479c1cd754d78a01827e126b7aca58e05
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2021-06-20 05:35:11 -0300

    Update using-plots.jl

--- using-plots.jl
+++ using-plots.jl
@@ -3,6 +3,6 @@ x = 1:10
 f(x) = x^2 - 7x
 y = f.(x)
 
-gr()                                # use the GR bacckend
+gr()                                # use the GR backend
 plot(x, y, label="line")            # plot a line
 scatter!(x, y, label="points")      # mutate previous plot (!) and add points at same chart
