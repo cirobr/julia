eb85c5a34f9ec49aaa61359e60284368b490cf4d
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2021-12-11 05:36:32 -0300

    update jupyter notebooks

--- numpy.jl
+++
@@ -1,5 +0,0 @@
-using PyCall
-np = pyimport("numpy")
-a = np.ones(100)
-s = np.sum(a)
-println(s)
--- using-plots.jl
+++
@@ -1,8 +0,0 @@
-using Plots
-x = 1:10
-f(x) = x^2 - 7x
-y = f.(x)
-
-gr()                                # use the GR backend
-plot(x, y, label="line")            # plot a line
-scatter!(x, y, label="points")      # mutate previous plot (!) and add points at same chart
