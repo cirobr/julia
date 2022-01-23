20905027fc0264ba6367aa45f984f64817496736
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-01-23 20:52:03 -0300

    ep3 update

---
+++ pkgAdd.jl
@@ -0,0 +1,18 @@
+# add packages
+using Pkg
+Pkg.add("CUDA"); using CUDA
+Pkg.add("Flux"); using Flux
+Pkg.add("Images"); using Images
+Pkg.add("MLJ"); using MLJ
+
+Pkg.add("LinearAlgebra"); using LinearAlgebra
+Pkg.add("Metrics"); using Metrics
+Pkg.add("Random"); using Random
+Pkg.add("StatsBase"); using StatsBase
+Pkg.add("Distributions"); using Distributions
+
+Pkg.add("Plots"); using Plots
+Pkg.add("Printf"); using Printf
+
+Pkg.add("CSV"); using CSV
+Pkg.add("DataFrames"); using DataFrames
