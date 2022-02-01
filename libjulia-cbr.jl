eb88055efc5f86f8699e4afb0e909c8fabc1cd0f
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-02-01 12:22:43 -0300

    rename libjulia-cbr

--- libjulia-cbr.jl
+++ libjulia-cbr.jl
@@ -1,4 +1,5 @@
 # libraries
+using StatsBase
 using DataFrames
 using MLDataUtils
 
@@ -19,17 +20,6 @@ vector2Image(vec, h, v) = reshape(Float64.(vec), (h, v))
 
 
 
-function shuffleRowMatrix(X, Y)
-    ndim = size(Y)[1]
-    ind = randperm(ndim)   # shuffle array indices
-    shuffledX = X[ind, :]
-    shuffledY = trainY[ind]
-    
-    return (shuffledX, shuffledY)
-end
-
-
-
 function rescaleByColumns(X)
     # using StatsBase
     X = Float64.(X)
@@ -44,6 +34,17 @@ function rescaleByRows(X)
     X = Float64.(X)
     dt = StatsBase.fit(ZScoreTransform, X; dims=2, center=true, scale=true)
     rescaledX = StatsBase.transform(dt, X)
+end
+
+
+
+function shuffleRowMatrix(X, Y)
+    ndim = size(Y)[1]
+    ind = randperm(ndim)   # shuffle array indices
+    shuffledX = X[ind, :]
+    shuffledY = Y[ind]
+    
+    return (shuffledX, shuffledY)
 end
 
 
