199be6297b751cd4df9427856549d4493bbf1f11
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-02-03 09:54:55 -0300

    create julia-setup folder

---
+++ julia-setup/ai-libraries.jl
@@ -0,0 +1,19 @@
+# libraries
+using Flux              # the julia ml library
+using Images            # image processing and machine vision for julia
+using MLJ               # make_blobs, rmse, confmat, f1score, coerce
+using MLDataUtils       # label, nlabel, labelfreq
+using MLDatasets        # mnist
+
+using LinearAlgebra     # pinv pseudo-inverse matrix
+using Metrics           # r2-score
+using Random
+using StatsBase         # standardize (normalization)
+using Distributions
+
+using Plots; gr()
+using StatsPlots
+using Printf
+
+using CSV
+using DataFrames
