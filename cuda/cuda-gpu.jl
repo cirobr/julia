d5f18bfd5e2385ffa2101ad2b996501f19b34605
cirobr <ciro.rosa@alumni.usp.br>
2022-02-05 00:35:48 -0300

    cuda solved

--- cuda/cuda-gpu.jl
+++ cuda/cuda-gpu.jl
@@ -4,13 +4,12 @@ N = 10000
 
 a_d = CuArray{Float32}(undef, (N, N))
 b_d = CuArray{Float32}(undef, (N, N))
-c_d = CuArray{Float32}(undef, (N, N))
 
 for i in 1:10
-    global a_d = randn(N, N)
-    global b_d = randn(N, N)
+    randn!(a_d)
+    randn!(b_d)
 
-    global c_d = a_d * b_d
+    c_d = a_d * b_d
 end
 
 global a_d = nothing
