8e960fdae5be43930a15d3d6ddcd7bfed0fe8243
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-02-06 07:26:36 -0300

    cuda-gpu

--- cuda/cuda-gpu.jl
+++ cuda/cuda-gpu.jl
@@ -4,12 +4,13 @@ N = 10000
 
 a_d = CuArray{Float32}(undef, (N, N))
 b_d = CuArray{Float32}(undef, (N, N))
+c_d = CuArray{Float32}(undef, (N, N))
 
 for i in 1:10
     randn!(a_d)
     randn!(b_d)
 
-    c_d = a_d * b_d
+    global c_d = a_d * b_d
 end
 
 global a_d = nothing
