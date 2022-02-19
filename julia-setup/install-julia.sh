34051190249de52fde93df7122e16af9d9c531f9
cirobr <ciro.rosa@alumni.usp.br>
2022-02-18 22:28:39 -0300

    update install

--- julia-setup/install-julia.sh
+++ julia-setup/install-julia.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 
 pip install jill -U
-# jill install 1.4.1   # default ubuntu 20.04
+jill install 1.4.1   # default ubuntu 20.04
 jill install 1.6.5   # lts 19-dec-2021
-jill install 1.7.2   # stable 06-feb-2022
+#jill install 1.7.2   # stable 06-feb-2022
