9179ed1691f7ebe6e1a54395104d0ff087198113
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-03-13 09:05:57 -0300

    added sorted file names

--- libjulia-cbr.jl
+++ libjulia-cbr.jl
@@ -65,10 +65,12 @@ end
 
 
 
-function filesWithinFolder(folder, ext)
+function filesWithinFolder(folder, prefix, ext)
+    cutoffName = length(prefix) + 1
     files = readdir(folder)
     extFiles = [file for file in files if occursin( ext, file )]
-    extFiles = string.(folder, "/", extFiles)
+    sortedExtFiles = sort( [ parse(Int, file[cutoffName : end-4])  for file in extFiles ] )
+    sortedExtFiles = string.(folder, "/", prefix, sortedExtFiles, ext)
 end
 
 
