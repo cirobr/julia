e34b18794400deb21c979a3f6500ee9cca2cb782
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-04-02 17:24:53 -0300

    Update libjulia-cbr.jl

--- libjulia-cbr.jl
+++ libjulia-cbr.jl
@@ -2,10 +2,11 @@
 using StatsBase
 using DataFrames
 using MLDataUtils
+using OpenCV; const cv = OpenCV
 
 using PyCall
 rs  = pyimport("pyrealsense2")
-cv2 = pyimport("cv2")            # video playback only
+
 
 
 """
@@ -65,8 +66,85 @@ Converts a vector "vec" in a matrix of "h" lines by "v" columns.
 vector2Image(vec, h, v) = reshape(Float32.(vec), (h, v))
 
 
+rgb2CHW(img) = Images.channelview(img)
+rgb2HWC(img) = Images.permutedims(rgb2CHW(img), (2, 3, 1))
+rgb2CWH(img) = Images.permutedims(rgb2CHW(img), (1, 3, 2))
+
+
+
 """
-Centers and Rescales predictors on input matrix by columns.
+rgb2opencv(img)
+
+Converts images from RGB-2D to OpenCV type of representation.
+"""
+function rgb2opencv(img)
+    img2 = rgb2CWH(img)
+    img2 = Float32.(img2)
+    #img2 = cv.Mat(img2)
+end
+
+
+
+"""
+hex2rgb(img)
+
+Converts images from hex-3D-array to RGB-2D representation.
+"""
+function hex2rgb(img)
+    img2 = Float32.(img) ./ Float32(255.0)
+    img2 = RGB.(img2[:,:,1], img2[:,:,2], img2[:,:,3])
+end
+
+
+
+"""
+hex2uint8(img)
+
+Converts images from RGB-2D to uint8 representation.
+"""
+function rgb2uint8(img)
+    img2 = rgb2HWC(img)
+    img2 = reinterpret(UInt8, img2) .|> UInt8    
+end
+
+
+
+"""
+cvShowImage(imageFullPath, windowName)
+
+Uses OpenCV to show a single image on a window.
+"""
+function cvShowImage(imageFullPath, windowName)
+    img = cv.imread(imageFullPath)   # no need to convert file, read with OpenCV
+    cv.imshow(windowName, img)
+    cv.waitKey(Int32(0))
+    cv.destroyWindow(windowName)
+end
+
+
+
+"""
+cvPlayImageBuffer(imageBuffer, windowName, framesPerSecond)
+
+Uses OpenCV to play a sequence of RGB-2D images in a buffer as video, on a window.
+"""
+function cvPlayImageBuffer(imageBuffer, windowName, framesPerSecond)
+    frameInterval_ms = trunc( Int32, Float32(1.0 / framesPerSecond * 1000.0) )
+
+    for img in imageBuffer
+        img = rgb2opencv(img)
+        cv.imshow(windowName, img)
+        cv.waitKey(frameInterval_ms)
+    end
+    cv.destroyWindow(windowName)
+end
+
+
+
+"""
+rescaleByColumns(X::Matrix)
+
+Centers and Rescales predictors from input matrix by columns.
 """
 function rescaleByColumns(X::Matrix)
     # using StatsBase
@@ -78,7 +156,9 @@ end
 
 
 """
-Centers and Rescales predictors on input matrix by rows.
+rescaleByRows(X::Matrix)
+
+Centers and Rescales predictors from input matrix by rows.
 """
 function rescaleByRows(X::Matrix)
     # using StatsBase
@@ -110,61 +190,3 @@ end
 
 
 
-img_CHW(img) = Images.channelview(img)
-img_HWC(img) = Images.permutedims(img_CHW(img), (2, 3, 1))
-img_CWH(img) = Images.permutedims(img_CHW(img), (1, 3, 2))
-
-
-
-"""
-hex2RGB(img)
-
-Converts images from hex-3D-array to RGB-2D-matrix representation.
-"""
-function hex2RGB(img)
-    img2 = Float32.(img) ./ Float32(255.0)
-    img2 = RGB.(img2[:,:,1], img2[:,:,2], img2[:,:,3])
-end
-
-
-
-"""
-hex2uint8(img)
-
-Converts images from hex to uint8 representation.
-"""
-function hex2uint8(img)
-    img2 = img_HWC(img)
-    img2 = reinterpret(UInt8, img2) .|> UInt8    
-end
-
-
-
-"""
-cv2ShowImage(imageFullPath, windowName)
-
-Uses OpenCV to show a single image on a window.
-"""
-function cv2ShowImage(imageFullPath, windowName)
-    img = cv2.imread(imageFullPath)
-    cv2.imshow(windowName, img)
-    cv2.waitKey(Int32(0))
-    cv2.destroyWindow(windowName)
-end
-
-
-
-"""
-cv2PlayImageBuffer(imageBuffer, windowName, framesPerSecond)
-
-Uses OpenCV to play a sequence of images in a buffer as video on a window.
-"""
-function cv2PlayImageBuffer(imageBuffer, windowName, framesPerSecond)
-    frameInterval_ms = trunc( Int16, Float32(1.0 / framesPerSecond * 1000.0) )
-
-    for img in imageBuffer
-        cv2.imshow(windowName, img)
-        cv2.waitKey(frameInterval_ms)
-    end
-    cv2.destroyWindow(windowName);
-end
