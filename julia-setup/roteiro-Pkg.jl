f243d0a499d31149ccad761105db2e9b1adad4d1
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-04-03 17:22:59 -0300

    Update roteiro-Pkg.jl

--- julia-setup/roteiro-Pkg.jl
+++ julia-setup/roteiro-Pkg.jl
@@ -1,6 +1,20 @@
-libraryName = "libImg"
-
+# libraries
+using Pkg
 using PkgTemplates
-t=Template(user="cirobr", authors="Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
-           julia=v"1.6.5", dir="~/libjulia")
+
+# path
+libPath = "/home/ciro/projects/libjulia"
+libraryName = "libImageArrays.jl"
+libString = string(libPath, "/", libraryName)
+
+# create package
+t=Template(user="cirobr",   # github username
+           authors="Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
+           julia=v"1.6.5",
+           dir=libPath)   #libPath[1 : end-3])
 t(libraryName)
+
+# add dependencies
+# Pkg.activate(libString)
+# Pkg.add("Images") 
+# Pkg.add("OpenCV") 
