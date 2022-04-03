f136e0f2b5a5867a56b22462796f5d801c43aed8
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-04-03 11:05:07 -0300

    Update roteiro-Pkg.jl

--- julia-setup/roteiro-Pkg.jl
+++ julia-setup/roteiro-Pkg.jl
@@ -1,6 +1,6 @@
-libraryName = "lib2"
+libraryName = "libImg"
 
 using PkgTemplates
 t=Template(user="cirobr", authors="Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
-           julia=v"1.6.5", dir="~/projects/dev")
+           julia=v"1.6.5", dir="~/libjulia")
 t(libraryName)
