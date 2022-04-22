a4510a54b3120ba21b97a780340b30c6699a6ee0
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-04-22 14:22:01 -0300

    update pkg-roteiro

---
+++ julia-setup/pkg-roteiro.jl
@@ -0,0 +1,20 @@
+# libraries
+using Pkg
+using PkgTemplates
+
+# library name
+libName  = "PreprocessingArrays.jl"
+fullpath = "~/.julia/dev"
+lib      = libName[1:end-3]
+
+# create package
+t=Template(user="cirobr",   # github username
+           authors="Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
+           julia=v"1.6.6",
+           dir=fullpath)
+t(lib)
+
+# add dependencies
+# Pkg.activate(lib)
+# Pkg.add("Images") 
+# Pkg.add("OpenCV") 
