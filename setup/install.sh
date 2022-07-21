a27a0ad415b8d46d33e8c849ca3eb574c3b46db7
Ciro B Rosa <ciro.rosa@alumni.usp.br>
2022-07-21 12:23:08 -0300

    update Julia 1.6.7

--- setup/install.sh
+++ setup/install.sh
@@ -2,6 +2,5 @@
 #!/bin/bash
 
 pip install jill -U
-jill install 1.6.6   # lts
+jill install 1.6.7   # lts 19-jul-2022
 #jill install 1.7.2   # stable 06-feb-2022
-#jill install 1.4.1   # default ubuntu 20.04
