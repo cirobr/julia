c804422316a308459a61f64ebfb0d29a043781a7
cirobr <ciro.rosa@alumni.usp.br>
2022-10-26 09:39:17 -0300

    added folder rename

--- setup/create-env.jl
+++ setup/create-env.jl
@@ -1,20 +1,23 @@
+### run this code from the "projects" folder ###
+
 # libraries
 using Pkg
 using PkgTemplates
 
 # library name
-libName  = "PreprocessingImages.jl"
-fullpath = "~/projects/libjulia"
-lib      = libName[1:end-3]
+lib      = "UnetFlux"
+fullpath = "~/projects"
 
 # create package
 t=Template(user="cirobr",   # github username
            authors="Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
-           julia=v"1.6.6",
+           julia=v"1.6.7",
            dir=fullpath)
 t(lib)
 
 # add dependencies
-# Pkg.activate(lib)
-# Pkg.add("Images") 
-# Pkg.add("OpenCV") 
+Pkg.activate(lib)
+Pkg.add("Flux") 
+Pkg.add("CUDA") 
+
+mv(libName, libName * ".jl")
