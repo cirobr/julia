08e8de26958942c01bda1a200924c65273b92019
cirobr <ciro.rosa@alumni.usp.br>
2023-01-02 06:11:23 -0300

    bug fix

--- setup/create-env.jl
+++ setup/create-env.jl
@@ -1,23 +1,22 @@
-### run this code from the "projects" folder ###
-
 # libraries
 using Pkg
 using PkgTemplates
 
 # library name
-lib      = "UnetFlux"
-fullpath = "~/projects"
+lib      = "Mylib"   # replace by the actual package name
+path     = expanduser("~/projects/")
+fullpath = path * lib
 
 # create package
 t=Template(user="cirobr",   # github username
            authors="Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
-           julia=v"1.6.7",
-           dir=fullpath)
+           julia=v"1.8.2",
+           dir=path)
 t(lib)
 
 # add dependencies
-Pkg.activate(lib)
+Pkg.activate(fullpath)
 Pkg.add("Flux") 
 Pkg.add("CUDA") 
 
-mv(lib, lib * ".jl")
+mv(fullpath, fullpath*".jl")
