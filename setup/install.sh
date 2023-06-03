1a017f9598a9d4493dbba173b9853b00a74e0637
cirobr <ciro.rosa@alumni.usp.br>
2023-06-03 15:09:25 +0000

    deleted:    setup/create-env.jl 	deleted:    setup/dot-bashrc 	deleted:    setup/install.sh 	deleted:    setup/pkgs.jl 	deleted:    setup/uninst.sh

--- setup/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://julialang.org/downloads/platform/
-pip install jill -U
-jill install 1.9.0
-
-# sudo snap install julia --classic   # not suitable for VSCode
--- setup/uninst.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo snap remove julia
-rm -rf ~/.julia
-rm -rf ~/packages/julias
-rm ~/.local/bin/julia*
